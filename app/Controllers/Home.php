610803c6070dab10fd3b6320d3bd592314cbddf8
fanzalrivaldi <muh.fanzal@mhs.usk.ac.id>
2026-01-27 16:19:44 +0700

    update news landing

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -2,95 +2,55 @@
 
 namespace App\Controllers;
 
+use App\Models\BeritaModel;
+
 class Home extends BaseController
 {
     public function index()
     {
+        $beritaModel = new BeritaModel();
+
+        // contoh: 3 berita terbaru aktif untuk landing page
+        $latestNews = $beritaModel
+            ->where('is_active', 1)
+            ->orderBy('created_at', 'DESC')
+            ->findAll(3);
+
         $data = [
             'title' => 'Beranda - KORPRI Aceh',
+
             'slides' => [
-            [
-                'image' => base_url('assets/img/slide-1.JPG'),
-                'caption' => 'Upacara KORPRI Aceh',
-            ],
-            [
-                'image' => base_url('assets/img/slide-2.JPG'),
-                'caption' => 'MUSPROF KORPRI ACEH 2025',
-            ],
-            [
-                'image' => base_url('assets/img/slide-3.JPG'),
-                'caption' => 'Pornas KORPRI XVII',
-            ],
-        ],
-
-        'news' => [
-                [
-                    'image' => base_url('assets/img/slide-1.JPG'),
-                    'title' => 'Berita 1',
-                    'excerpt' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-                    'date' => '14 Januari 2025'
-                ],
-                [
-                    'image' => base_url('assets/img/slide-2.JPG'),
-                    'title' => 'Berita 2',
-                    'excerpt' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-                    'date' => '14 Januari 2025'
-                ],
-                [
-                    'image' => base_url('assets/img/slide-3.JPG'),
-                    'title' => 'Berita 3',
-                    'excerpt' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-                    'date' => '14 Januari 2025'
-                ],
+                ['image' => base_url('assets/img/slide-1.JPG'), 'caption' => 'Upacara KORPRI Aceh'],
+                ['image' => base_url('assets/img/slide-2.JPG'), 'caption' => 'MUSPROF KORPRI ACEH 2025'],
+                ['image' => base_url('assets/img/slide-3.JPG'), 'caption' => 'Pornas KORPRI XVII'],
             ],
 
-        'pengumuman' => [
+            // GANTI news statis -> dari DB
+            'news' => $latestNews,
+
+            'pengumuman' => [
                 ['title' => 'Pengumuman 1', 'link' => '/pengumuman/1'],
                 ['title' => 'Pengumuman 2', 'link' => '/pengumuman/2'],
                 ['title' => 'Pengumuman 3', 'link' => '/pengumuman/3'],
             ],
 
-
-            'tentang_korpri' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+             'tentang_korpri' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
 
             'programs' => [
-                [
-                    'icon' => 'fas fa-gavel',
-                    'title' => 'DIGITALISAI BIROKRASI',
-                    'description' => 'Layanan konsultasi dan bantuan hukum untuk anggota KORPRI',
-                    'link' => '/program/bantuan-hukum'
-                ],
-                [
-                    'icon' => 'fas fa-hand-holding-usd',
-                    'title' => 'PENGUATAN NILAI ASN',
-                    'description' => 'Program peningkatan kesejahteraan anggota KORPRI',
-                    'link' => '/program/kesejahteraan'
-                ],
-                [
-                    'icon' => 'fas fa-building',
-                    'title' => 'PERLINDUNGAN KARIR',
-                    'description' => 'Koperasi KORPRI untuk kemajuan ekonomi anggota',
-                    'link' => '/program/koperasi'
-                ],
-                [
-                    'icon' => 'fas fa-store',
-                    'title' => 'KESEJAHTERAAN ASN',
-                    'description' => 'Pemberdayaan UMKM untuk anggota KORPRI',
-                    'link' => '/program/umkm'
-                ],
+                ['icon' => 'fas fa-gavel', 'title' => 'DIGITALISAI BIROKRASI', 'description' => '...', 'link' => '/program/bantuan-hukum'],
+                ['icon' => 'fas fa-hand-holding-usd', 'title' => 'PENGUATAN NILAI ASN', 'description' => '...', 'link' => '/program/kesejahteraan'],
+                ['icon' => 'fas fa-building', 'title' => 'PERLINDUNGAN KARIR', 'description' => '...', 'link' => '/program/koperasi'],
+                ['icon' => 'fas fa-store', 'title' => 'KESEJAHTERAAN ASN', 'description' => '...', 'link' => '/program/umkm'],
             ],
 
-            
-            
-        'gallery' => [
-            ['image' => base_url('assets/img/slide-1.JPG'), 'title' => 'Kegiatan 1'],
-            ['image' => base_url('assets/img/slide-2.JPG'), 'title' => 'Kegiatan 2'],
-            ['image' => base_url('assets/img/slide-3.JPG'), 'title' => 'Kegiatan 3'],
-            ['image' => base_url('assets/img/slide-4.JPG'), 'title' => 'Kegiatan 4'],
-            ['image' => base_url('assets/img/slide-5.JPG'), 'title' => 'Kegiatan 5'],
-        ]
-
+            'gallery' => [
+                ['image' => base_url('assets/img/slide-1.JPG'), 'title' => 'Kegiatan 1'],
+                ['image' => base_url('assets/img/slide-2.JPG'), 'title' => 'Kegiatan 2'],
+                ['image' => base_url('assets/img/slide-3.JPG'), 'title' => 'Kegiatan 3'],
+                ['image' => base_url('assets/img/slide-4.JPG'), 'title' => 'Kegiatan 4'],
+                ['image' => base_url('assets/img/slide-5.JPG'), 'title' => 'Kegiatan 5'],
+            ],
         ];
 
         return view('home', $data);

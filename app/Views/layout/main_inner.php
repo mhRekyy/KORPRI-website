5f43518665847b70ba5137fd9fd2b59fdcd6dee5
fanzalrivaldi <muh.fanzal@mhs.usk.ac.id>
2026-02-11 19:58:12 +0700

    initial commit

--- app/Views/layout/main_inner.php
+++ app/Views/layout/main_inner.php
@@ -6,19 +6,17 @@
 
   <title><?= esc($pageTitle ?? 'KORPRI Aceh') ?></title>
 
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+  <link rel="stylesheet" href="<?= base_url('assets/css/bootstrap.min.css') ?>">
 
   <!-- CSS komponen title_pages -->
   <link rel="stylesheet" href="<?= base_url('assets/css/title_pages.css') ?>">
 
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+  <link rel="stylesheet" href="<?= base_url('assets/fontawesome/css/all.min.css') ?>">
+  <link rel="stylesheet" href="<?= base_url('assets/bootstrap-icons/bootstrap-icons.css') ?>">
   <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800&display=swap">
 
   <!-- CSS Footer -->
   <link rel="stylesheet" href="<?= base_url('assets/css/footer.css') ?>">
-
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
   
 </head>
 
